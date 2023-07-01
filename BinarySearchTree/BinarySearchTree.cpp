ac0501573690b84b0f6548b350b7dffb37a8369c
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-07-01 12:19:01 +0800

    menambahkan void search

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -56,6 +56,21 @@ public:
 		}
 	}
 
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		// this function searches the currentNode of the specified Node as well as the current Node of it's parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
 	
 };
 
